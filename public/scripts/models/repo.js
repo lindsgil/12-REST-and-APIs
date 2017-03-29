162edad01a492aa68f57013a40ad8d93089ea945
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-29 16:37:21 -0700

    first 3 todos completed

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,7 +7,20 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    $ajax({
+      url:'https://api.github.com/user/repos?type=owner',
+      method: 'GET',
+      headers:{
+        Authorization: `token ${githubToken}`
+      }
+    })
+    .then(
+      data => data.map(repo => $('#repo-template').append(`${repo.name}`)),
 
+      err => {
+        console.log(err);
+      }
+    )
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
